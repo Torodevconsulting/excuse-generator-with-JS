c85b474291376b8d363bbd8f6cb66ac463a033da
Isaac Toro <torodevelopmentbcn@gmail.com>
2026-01-22 12:51:40 +0000

    Excuse Generator done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    //These are my arrays with the parts of the excuse
+    let who = ['Rocko ', 'My Mom ', 'The slenderman ', 'The witch of blair '];
+    let action = ['ate ', 'spelled ', 'digged ', 'poked '];
+    let what = ['my eye ', 'your butt ', 'her armpit ', 'the dough '];
+    let when = ['when I was running', 'while i was excercising', 'when i was fighting the crime', 'in the church', 'while i was programing'];
+    
+    //This is how i will generate random indexes for every single array
+    let randomWho = Math.floor(Math.random()* who.length);
+    let randomAction = Math.floor(Math.random()* action.length);
+    let randomWhat = Math.floor(Math.random()* what.length);
+    let randomWhen = Math.floor(Math.random()* when.length);
+
+    //Now will create the excuse concatenating the ramdom elements
+    let excuse = who[randomWho] + ' ' + action[randomAction] + '' + what[randomWhat] + '' + when[randomWhen];
+
+    document.getElementById('excuse').innerHTML = excuse;
+
 };
