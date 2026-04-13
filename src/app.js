5e86c308143182d96e8658d37f8077db44103efe
Isaac Toro <torodevelopmentbcn@gmail.com>
2026-04-13 18:55:11 +0000

    refractor function generated to refresh the function and delete the reload function.

--- src/app.js
+++ src/app.js
@@ -3,26 +3,21 @@ import "./style.css";
 
 window.onload = function() {
     //These are my arrays with the parts of the excuse
-    let who = ['Rocko ', 'My Mom ', 'The slenderman ', 'The witch of blair', 'The ghost of my dead grandma'];
-    let action = ['ate ', 'spelled ', 'digged ', 'poked '];
-    let what = ['my eye ', 'your butt ', 'her armpit ', 'the dough '];
-    let when = ['when I was running', 'while i was excercising', 'when i was fighting the crime', 'in the church', 'while i was programing'];
+    const who = ['Rocko ', 'My Mom ', 'The slenderman ', 'The witch of blair', 'The ghost of my dead grandma'];
+    const action = ['ate ', 'spelled ', 'digged ', 'poked '];
+    const what = ['my eye ', 'your butt ', 'her armpit ', 'the dough '];
+    const when = ['when I was running', 'while i was excercising', 'when i was fighting the crime', 'in the church', 'while i was programing'];
     
     //This is how i will generate random indexes for every single array
+    function generateExcuse() {
     let randomWho = Math.floor(Math.random()* who.length);
     let randomAction = Math.floor(Math.random()* action.length);
     let randomWhat = Math.floor(Math.random()* what.length);
     let randomWhen = Math.floor(Math.random()* when.length);
-
-    //Now will create the excuse concatenating the ramdom elements
     let excuse = who[randomWho] + ' ' + action[randomAction] + '' + what[randomWhat] + '' + when[randomWhen];
-
     document.getElementById('excuse').innerHTML = excuse;
+}
+    generateExcuse();
+    document.getElementById('excuseBtn').addEventListener('click', generateExcuse);   
+
 };
-
-const button = document.getElementById('excuseBtn');
-button.addEventListener('click', function() {
-    //location reload will reload the page and generate a new excuse
-    location.reload();
-});
-
